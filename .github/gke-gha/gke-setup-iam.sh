7890de4f6fd44cc1bfadcfdd91985ab7d502c261
kseto06 <kaden.seto06@gmail.com>
2026-06-07 13:37:24 -0400

    add: IAM admin permissions

--- .github/gke-gha/gke-setup-iam.sh
+++ .github/gke-gha/gke-setup-iam.sh
@@ -19,3 +19,9 @@ PROJECT_NUMBER="$(
 NODE_SERVICE_ACCOUNT="${PROJECT_NUMBER}-compute@developer.gserviceaccount.com"
 
 gcloud iam service-accounts add-iam-policy-binding "$NODE_SERVICE_ACCOUNT" --project="$PROJECT_ID" --member="serviceAccount:${GITHUB_ACTIONS_SA}" --role="roles/iam.serviceAccountUser"
+
+PROJECT_ID="project-0c6e9a84-c914-4d2f-ace"
+
+echo "$GITHUB_ACTIONS_SA"
+
+gcloud projects add-iam-policy-binding "$PROJECT_ID" --member="serviceAccount:${GITHUB_ACTIONS_SA}" --role="roles/resourcemanager.projectIamAdmin"
